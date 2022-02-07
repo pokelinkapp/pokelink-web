d6e591b4f427aaa23465189ec7abda964861ec20
Daniel Noel-Davies <NoelDavies@cybershade.org>
2022-02-07 23:20:13 +0000

    Fix alternate form in helpers.js

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -45,7 +45,7 @@ function transformPokemon(pokemon) {
 
   // handle forms
   if (settings.pokeImg.ignoreForms === false) {
-    if (pokemon.hasOwnProperty('alternateForm') && pokemon.alternateForm pokemon.alternateForm.len > 0) {
+    if (pokemon.hasOwnProperty('alternateForm') && pokemon.alternateForm && pokemon.alternateForm.len > 0) {
       try {
         switch (pokemon.alternateForm) {
           case 'gmax':
